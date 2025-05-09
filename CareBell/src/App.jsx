119e4d2e01eeac2dde5da5cd76b11b5495a4c386
yBraude <mor.hodaya.maman@e.braude.ac.il>
2025-05-09 11:43:46 +0300

    updated App.jsx

--- CareBell/src/App.jsx
+++ CareBell/src/App.jsx
@@ -1,18 +1,27 @@
+// src/App.jsx
 import React from "react";
-import Header from "./components/Header";
-import LeftSide from "./components/LeftSide";
-import RightSide from "./components/RightSide";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header        from "./components/Header";
+import LeftSide      from "./components/LeftSide";
+import RightSide     from "./components/RightSide";
 
 export default function App() {
   return (
-    <div className="w-full max-w-screen-lg mx-auto p-4 min-h-screen flex flex-col" 
-         style={{ fontSize: "var(--font-size-base,22px)" }}>
-      <Header />
-      <div id="mainContent" className="flex-1 flex flex-col md:flex-row gap-8">
-        <LeftSide />
-        <RightSide />
+    <BrowserRouter>
+      <div
+        className="w-full max-w-screen-lg mx-auto p-4 min-h-screen flex flex-col"
+        style={{ fontSize: "var(--font-size-base,22px)" }}
+      >
+        <Header />
+
+        <div id="mainContent" className="flex-1 flex flex-col md:flex-row gap-8">
+          <LeftSide />
+
+          {/* זה המקום שבו הכפתורים נשארים */}
+          <RightSide />
+        </div>
       </div>
-      {/* …later your mainContent goes here… */}
-    </div>
-  );
+    </BrowserRouter>
+);
 }
