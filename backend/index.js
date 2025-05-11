5351fd0d6e97b7fc021d6d89fd0c51889e65c735
ashrafatshy7 <ashrafatshy7@gmail.com>
2025-05-11 11:29:03 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const https = require('https');
 const fs = require('fs');
 const { Server } = require('socket.io');
 
-
+// Your routes
 const userRoute = require('./routes/users');
 const contactRoute = require('./routes/contacts');
 const foodRoute = require('./routes/foods');
@@ -14,41 +14,33 @@ const medicationRoute = require('./routes/medications');
 const bellaReminderRoute = require('./routes/bellaReminders');
 const newsRoute = require('./routes/news');
 
-
-const options = {
-  key: fs.readFileSync('localhost+3-key.pem'),
-  cert: fs.readFileSync('localhost+3.pem')
-};
-
 const app = express();
-const server = https.createServer(options, app);
+const server = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/carebell.online/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/carebell.online/fullchain.pem')
+}, app);
+
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: '*',
+    methods: ['GET', 'POST'],
     credentials: true
   },
   transports: ['websocket', 'polling']
 });
 
-io.on("connection_error", (err) => {
+io.on('connection_error', (err) => {
   console.log(`Connection error: ${err.message}`);
 });
 
-const PORT = 4000;
-
-
-
+const PORT = 443;
 
 app.use(cors());
 app.use(express.json());
 
-
 mongoose.connect('mongodb://localhost:27017/CareBell')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
-
-
 
 app.use('/users', userRoute);
 app.use('/contacts', contactRoute);
@@ -57,20 +49,12 @@ app.use('/medications', medicationRoute);
 app.use('/bellaReminders', bellaReminderRoute);
 app.use('/news', newsRoute);
 
-
-
 app.get('/', (req, res) =>{
     res.send('asdfsfd');
 });
 
-
-
 require('./sockets')(io);
 
-
-
-
-
 server.listen(PORT, () => {
-  console.log(`HTTPS server started`);
+  console.log('HTTPS server started on https://carebell.online');
 });
