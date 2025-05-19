8708bb84a798e4075423b32359e770a87c00993f
Leo Feldman <leolight214@gmail.com>
2025-05-19 17:57:52 +0300

    fix to db address

--- backend/index.js
+++ backend/index.js
@@ -39,9 +39,13 @@ const PORT = 4443;
 app.use(cors()); 
 app.use(express.json()); 
 
-mongoose.connect('mongodb+srv://CareBell:vTDHDu9pHns9HNlw@cluster0.bqe7zge.mongodb.net') 
+mongoose.connect('mongodb+srv://CareBell:vTDHDu9pHns9HNlw@cluster0.bqe7zge.mongodb.net/CareBell') 
   .then(() => console.log('Connected to MongoDB')) 
   .catch(err => console.error('Could not connect to MongoDB:', err)); 
+
+  mongoose.connection.on('open', () => {
+    console.log('✅ MongoDB connected, DB name:', mongoose.connection.db.databaseName);
+  });
 
 app.use('/users', userRoute); 
 app.use('/contacts', contactRoute); 
